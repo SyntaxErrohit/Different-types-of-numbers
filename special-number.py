88842a9ceeaaf19430e802d9070268f306c716f6
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-17 22:33:53 +0530

    added alt

--- special-number.py
+++ special-number.py
@@ -14,3 +14,20 @@ if copy == total:
     print(copy, "is a special number")
 else:
     print(copy, "is not a special number")
+
+# Alternate solution
+n = int(input("Enter number: "))
+total = 0
+
+s = str(n)
+for i in s:
+    digit = int(i)
+    f = 1
+    for i in range(digit):
+        f *= i + 1
+    total += f
+
+if n == total:
+    print(n, "is an special number")
+else:
+    print(n, "is not an special number")
